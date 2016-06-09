07a6eb7ee2fec6bdd544c234542a445363693c61
leonoverweel <l.overweel@gmail.com>
2016-06-09 10:39:44 +0200

    Improve formatting and comments in main.cpp
    Translate some French comments, and make sure everything stays within the 120-column width boundary. Also add some additional comments and standardize the formatting of existing comments.

--- main.cpp
+++ main.cpp
@@ -19,34 +19,34 @@
 #include "traqueboule.h"
 #include "imageWriter.h"
 
-//This is the main application
-//Most of the code in here, does not need to be modified.
-//It is enough to take a look at the function "drawFrame",
-//in case you want to provide your own different drawing functions
-
+/**
+* This is the main application. Most of the code in here does not need to be modified. It is enough to take a look at 
+* the function "drawFrame", in case you want to provide your own different drawing functions.
+*/
 Vec3Df MyCameraPosition;
 
-//MyLightPositions stores all the light positions to use
-//for the ray tracing. Please notice, the light that is
-//used for the real-time rendering is NOT one of these,
-//but following the camera instead.
+/**
+* MyLightPositions stores all the light positions to use for the ray tracing. Please notice, the light that is used for 
+* the real-time rendering is NOT one of these, but following the camera instead.
+*/
 std::vector<Vec3Df> MyLightPositions;
 
-//Main mesh
-Mesh MyMesh;
-
-unsigned int WindowSize_X = 800;  // resolution X
-unsigned int WindowSize_Y = 800;  // resolution Y
+Mesh MyMesh;						// Main mesh
+unsigned int WindowSize_X = 800;	// X-resolution
+unsigned int WindowSize_Y = 800;	// Y-resolution
 
 /**
- * Main function, which is drawing an image (frame) on the screen
+ * Drawing function, which draws an image (frame) on the screen.
  */
 void drawFrame()
 {
 	yourDebugDraw();
 }
 
-//animation is called for every image on the screen once
+
+/**
+* Animation is called once for every image on the screen.
+*/
 void animate()
 {
 	MyCameraPosition = getCameraPosition();
@@ -58,29 +58,29 @@ void reshape(int w, int h);
 void keyboard(unsigned char key, int x, int y);
 
 /**
- * Main Programme
+ * Main function.
  */
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 
-	//framebuffer setup
+	// Set up framebuffer.
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 
-	// positioning and size of window
+	// Positioning and size of window.
 	glutInitWindowPosition(200, 100);
 	glutInitWindowSize(WindowSize_X, WindowSize_Y);
 	glutCreateWindow(argv[0]);
 
-	//initialize viewpoint
+	// Initialize viewpoint.
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glTranslatef(0, 0, -4);
-	tbInitTransform();     // This is for the trackball, please ignore
-	tbHelp();             // idem
+	tbInitTransform();				// (This is for the trackball, please ignore.)
+	tbHelp();						// (idem.)
 	MyCameraPosition = getCameraPosition();
 
-	//activate the light following the camera
+	// Activate the light following the camera.
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_COLOR_MATERIAL);
@@ -88,25 +88,23 @@ int main(int argc, char** argv)
 	int MatSpec[4] = { 1, 1, 1, 1 };
 	glLightiv(GL_LIGHT0, GL_POSITION, LightPos);
 
-	//normals will be normalized in the graphics pipeline
+	// Normals will be normalized in the graphics pipeline.
 	glEnable(GL_NORMALIZE);
 
-	//clear color of the background is black.
+	// Clear color of the background is black.
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 
-	// Activate rendering modes
-	//activate depth test
+	// Activate rendering mode and activate depth test,
 	glEnable(GL_DEPTH_TEST);
 
-	//draw front-facing triangles filled
-	//and back-facing triangles as wires
+	// Draw front-facing triangles filled and back-facing triangles as wires.
 	glPolygonMode(GL_FRONT, GL_FILL);
 	glPolygonMode(GL_BACK, GL_LINE);
 
-	//interpolate vertex colors over the triangles
+	// Interpolate vertex colors over the triangles.
 	glShadeModel(GL_SMOOTH);
 
-	// glut setup... to ignore
+	// Glut setup. (ignore.)
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyboard);
 	glutDisplayFunc(display);
@@ -116,36 +114,48 @@ int main(int argc, char** argv)
 
 	init();
 
-	//main loop for glut... this just runs your application
+	// Main loop for glut. This just runs your application.
 	glutMainLoop();
 
-	return 0;  // execution never reaches this point
+	// Execution never reaches this point.
+	return 0;
 }
 
 /**
- * OpenGL setup - functions do not need to be changed!
- * you can SKIP AHEAD TO THE KEYBOARD FUNCTION
+ * OpenGL setup.
+ * 
+ * Functions do not need to be changed! You can skip ahead to the keyboard function.
  */
 
-//what to do before drawing an image
+/**
+* What to do before drawing an image.
+*/
 void display(void)
 {
-	glPushAttrib(GL_ALL_ATTRIB_BITS);//store GL state
+	// Store GL state.
+	glPushAttrib(GL_ALL_ATTRIB_BITS);
 
-	// Effacer tout
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear image
+	// Clear everything.
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glLoadIdentity();
 
-	tbVisuTransform(); // init trackball
+	// Init trackball.
+	tbVisuTransform();
 
-	drawFrame();    //actually draw
+	// Actually draw.
+	drawFrame();
 
-	glutSwapBuffers();//glut internal switch
-	glPopAttrib();//return to old GL state
+	// Glut internal switch.
+	glutSwapBuffers();
+
+	// Return to old GL state.
+	glPopAttrib();
 }
 
-//Window changes size
+/**
+* Window changes size.
+*/
 void reshape(int w, int h)
 {
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
@@ -157,15 +167,17 @@ void reshape(int w, int h)
 	glMatrixMode(GL_MODELVIEW);
 }
 
-//transform the x, y position on the screen into the corresponding 3D world position
+/**
+* Transform the x, y position on the screen into the corresponding 3D world position.
+*/
 void produceRay(int x_I, int y_I, Vec3Df * origin, Vec3Df * dest)
 {
 	int viewport[4];
 	double modelview[16];
 	double projection[16];
-	glGetDoublev(GL_MODELVIEW_MATRIX, modelview); //recuperer matrices
-	glGetDoublev(GL_PROJECTION_MATRIX, projection); //recuperer matrices
-	glGetIntegerv(GL_VIEWPORT, viewport);//viewport
+	glGetDoublev(GL_MODELVIEW_MATRIX, modelview);	// Recover matrices.
+	glGetDoublev(GL_PROJECTION_MATRIX, projection); // Recover matrices.
+	glGetIntegerv(GL_VIEWPORT, viewport);			// Viewport.
 	int y_new = viewport[3] - y_I;
 
 	double x, y, z;
@@ -174,36 +186,43 @@ void produceRay(int x_I, int y_I, Vec3Df * origin, Vec3Df * dest)
 	origin->p[0] = float(x);
 	origin->p[1] = float(y);
 	origin->p[2] = float(z);
+
 	gluUnProject(x_I, y_new, 1, modelview, projection, viewport, &x, &y, &z);
 	dest->p[0] = float(x);
 	dest->p[1] = float(y);
 	dest->p[2] = float(z);
 }
 
-// react to keyboard input
+/**
+* React to keyboard input.
+*/
 void keyboard(unsigned char key, int x, int y)
 {
 	printf("key %d pressed at %d,%d\n", key, x, y);
 	fflush(stdout);
 	switch (key)
 	{
-		//add/update a light based on the camera position.
+
+	// Press 'L'.
 	case 'L':
 		MyLightPositions.push_back(getCameraPosition());
 		break;
+
+	// Click 'l'.
 	case 'l':
 		MyLightPositions[MyLightPositions.size() - 1] = getCameraPosition();
 		break;
+
+	// Click 'r'.
 	case 'r':
 	{
-		//Pressing r will launch the raytracing.
+		// Pressing r will launch the raytracing.
 		cout << "Raytracing" << endl;
 
-		//Setup an image with the size of the current image.
+		// Setup an image with the size of the current image.
 		Image result(WindowSize_X, WindowSize_Y);
 
-		//produce the rays for each pixel, by first computing
-		//the rays for the corners of the frustum.
+		// Produce the rays for each pixel, by first computing the rays for the corners of the frustum.
 		Vec3Df origin00, dest00;
 		Vec3Df origin01, dest01;
 		Vec3Df origin10, dest10;
@@ -218,8 +237,7 @@ void keyboard(unsigned char key, int x, int y)
 		for (unsigned int y = 0; y < WindowSize_Y; ++y)
 			for (unsigned int x = 0; x < WindowSize_X; ++x)
 			{
-				//produce the rays for each pixel, by interpolating
-				//the four rays of the frustum corners.
+				// Produce the rays for each pixel, by interpolating the four rays of the frustum corners.
 				float xscale = 1.0f - float(x) / (WindowSize_X - 1);
 				float yscale = 1.0f - float(y) / (WindowSize_Y - 1);
 
@@ -228,21 +246,23 @@ void keyboard(unsigned char key, int x, int y)
 				dest = yscale*(xscale*dest00 + (1 - xscale)*dest10) +
 					(1 - yscale)*(xscale*dest01 + (1 - xscale)*dest11);
 
-				//launch raytracing for the given ray.
+				// Launch raytracing for the given ray.
 				Vec3Df rgb = performRayTracing(origin, dest);
 
-				//store the result in an image
+				// Store the result in an image.
 				result.setPixel(x, y, RGBValue(rgb[0], rgb[1], rgb[2]));
 			}
 
 		result.writeImage("result.ppm");
 		break;
 	}
-	case 27:     // touche ESC
+
+	// CLick 'Esc'.
+	case 27:
 		exit(0);
 	}
 
-	//produce the ray for the current mouse position
+	// Produce the ray for the current mouse position.
 	Vec3Df testRayOrigin, testRayDestination;
 	produceRay(x, y, &testRayOrigin, &testRayDestination);
 
